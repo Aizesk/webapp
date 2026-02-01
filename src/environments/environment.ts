38bb746dff8fa425281a1f5a2866131a4954d917
Enrique Lovera <kikelovera7@gmail.com>
2026-02-01 12:52:05 +0100

    Port updates and delete unused script

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   apiUrls: {
-    auth: 'http://localhost:8082/api/v1/auth',
-    users: 'http://localhost:8083/api/v1/users',
-    transactions: 'http://localhost:8080/api/v1/transactions',
-    reporting: 'http://localhost:8081/api/v1/reports',
+    auth: 'http://localhost:8081/api/v1/auth',
+    users: 'http://localhost:8082/api/v1/users',
+    transactions: 'http://localhost:8083/api/v1/transactions',
+    reporting: 'http://localhost:8087/api/v1/reports',
     subscriptions: 'http://localhost:8084/api/v1/subscriptions',
     platforms: 'http://localhost:8085/api/v1/connections',
     notifications: 'http://localhost:8086/api/v1/notifications'
