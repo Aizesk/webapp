707df050a016a87d787334618cb29e68516b8a69
Enrique Lovera <kikelovera7@gmail.com>
2026-02-21 19:17:08 +0100

    update terraform config

--- deployment/terraform/ecs-overrides.tf
+++ deployment/terraform/ecs-overrides.tf
@@ -102,13 +102,13 @@ resource "aws_ecs_task_definition" "platform_connection_service" {
       environment = [
         { name = "SPRING_PROFILES_ACTIVE", value = "prod" },
         { name = "SPRING_DATASOURCE_URL", value = "jdbc:mysql://${aws_db_instance.main.endpoint}/${var.db_name}?useSSL=true&requireSSL=true&allowPublicKeyRetrieval=true" },
-        { name = "CORS_ALLOWED_ORIGINS", value = "http://localhost:4200,https://app.aizesk.com,https://www.aizesk.com,http://${aws_s3_bucket_website_configuration.frontend.website_endpoint},http://${aws_lb.main.dns_name}" },
+        { name = "CORS_ALLOWED_ORIGINS", value = "http://localhost:4200,https://app.aizesk.com,https://www.aizesk.com,http://${aws_s3_bucket_website_configuration.frontend.website_endpoint},http://${aws_lb.main.dns_name},${aws_apigatewayv2_api.alb_proxy.api_endpoint}" },
         { name = "AUTH_SERVICE_URL", value = "http://${aws_lb.main.dns_name}" },
         { name = "USER_SERVICE_URL", value = "http://${aws_lb.main.dns_name}" },
         { name = "TRANSACTION_SERVICE_URL", value = "http://${aws_lb.main.dns_name}" },
         { name = "SUBSCRIPTION_SERVICE_URL", value = "http://${aws_lb.main.dns_name}" },
         { name = "FRONTEND_URL", value = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}" },
-        { name = "SHOPIFY_REDIRECT_URI", value = "http://${aws_lb.main.dns_name}/api/v1/platforms/callback/shopify" },
+        { name = "SHOPIFY_REDIRECT_URI", value = "${aws_apigatewayv2_api.alb_proxy.api_endpoint}/api/v1/platforms/callback/shopify" },
         { name = "SHOPIFY_API_VERSION", value = "2026-01" },
       ]
 
